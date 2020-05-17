dbf8c77b5a7289c7e57b189fff1b8773fddda987
xiaoyu.lin <xiaoyu.lin@epfl.ch>
2020-05-17 12:08:22 +0200

    draft

--- modules.py
+++ modules.py
@@ -4,6 +4,7 @@
 # @Author       : Xiaoyu LIN
 # @File         : modules.py
 # @Description  :
+import torch
 
 
 class Linear(object):
@@ -11,28 +12,43 @@ class Linear(object):
     Fully connected layer
     """
 
-    def forward(self, *input):
+    def __init__(self, in_dim, out_dim, epsilon):
+        self.parameters = [torch.empty(out_dim, in_dim), torch.empty(out_dim)]
+        self.parameters[0].normal_(0, epsilon)
+        self.parameters[1].normal_(0, epsilon)
+
+    def forward(self, input):
         """
 
         :param input:
         :return:
         """
-        raise NotImplementedError
+        self.input = input
+        output = input.matmul(self.parameters[0].t())
+        output += self.parameters[1]
+        return output
 
-    def backward(self, *gradwrtoutput):
+    def backward(self, gradwrtoutput):
         """
 
         :param gradwrtoutput:
         :return:
         """
-        raise NotImplementedError
+        self.gradwrtoutput = gradwrtoutput
+        return self.parameters[0].t().mv(gradwrtoutput)
+
+    def grad(self):
+
+        grad_wrt_weight = self.gradwrtoutput.view(-1, 1).mm(self.input.mean(0).view(1, -1))
+        grad_wrt_bias = self.gradwrtoutput
+        return [grad_wrt_weight, grad_wrt_bias]
 
     def param(self):
         """
 
         :return:
         """
-        return []
+        return self.parameters
 
 
 class Tanh(object):
@@ -40,14 +56,16 @@ class Tanh(object):
     Tanh function
     """
 
-    def forward(self, *input):
-        raise NotImplementedError
+    def forward(self, input):
+        self.input = input
+        return input.tanh()
 
-    def backward(self, *gradwrtoutput):
-        raise NotImplementedError
+    def backward(self, gradwrtoutput):
+        grad = 4 * (self.input.exp() + self.input.mul(-1).exp()).pow(-2).mean(0)
+        return grad * gradwrtoutput
 
     def param(self):
-        return []
+        return None
 
 
 class ReLU(object):
@@ -55,26 +73,58 @@ class ReLU(object):
     ReLu function
     """
 
-    def forward(self, *input):
-        raise NotImplementedError
+    def forward(self, input):
+        self.input = input
+        return input.relu()
 
-    def backward(self, *gradwrtoutput):
-        raise NotImplementedError
+    def backward(self, gradwrtoutput):
+        grad = torch.where(self.input > 0, torch.ones_like(self.input), torch.zeros_like(self.input)).mean(0)
+        return grad * gradwrtoutput
 
     def param(self):
-        return []
+        return None
 
 
 class Sequential(object):
 
-    def forward(self, *input):
-        raise NotImplementedError
+    def __init__(self, *layers):
 
-    def backward(self, *gradwrtoutput):
-        raise NotImplementedError
+        self.model = []
+        for layer in layers:
+            self.model.extend(layer)
+
+    def forward(self, input):
+        output = input
+        for layer in self.model:
+            output = layer.forward(output)
+        return output
+
+    def backward(self, gradwrtoutput):
+        output = gradwrtoutput
+        for layer in reversed(self.model):
+            output = layer.backward(output)
 
     def param(self):
-        return []
+        output = []
+        for layer in self.model:
+            if layer.param() is not None:
+                output.extend(layer.param())
+        return output
+
+    def gard(self):
+        output = []
+        for layer in self.model:
+            if layer.param() is not None:
+                output.extend(layer.grad())
+        return output
+
+    def update(self, parameters):
+        i = 0
+        for layer in self.model:
+            if layer.param() is not None:
+                for t in range(len(layer.param())):
+                    layer.parameters[t] = parameters[i]
+                    i += 1
 
 
 class LossMSE(object):
@@ -82,11 +132,15 @@ class LossMSE(object):
     compute the MSE loss.
     """
 
-    def forward(self, *input):
-        raise NotImplementedError
+    def forward(self, pred, label):
+        self.pred = pred
+        self.label = label
+        return (pred - label).pow(2).mean(0).sum()
 
-    def backward(self, *gradwrtoutput):
-        raise NotImplementedError
+    def backward(self):
+        return 2 * (self.pred - self.label).mean(0)
 
     def param(self):
-        return []
+        return None
+
+
